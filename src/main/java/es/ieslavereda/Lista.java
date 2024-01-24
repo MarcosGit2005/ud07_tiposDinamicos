c45cedb574a68d9550dd967fbe7da4b52c3e13fe
MarcosGit2020 <marrubdia@alu.edu.gva.es>
2024-01-24 19:19:55 +0100

    0.1

--- src/main/java/es/ieslavereda/Lista.java
+++ src/main/java/es/ieslavereda/Lista.java
@@ -9,6 +9,19 @@ public class Lista {
         cola=null;
         tamaño=0;
     }
+    public Lista(int[] elements){
+        this();
+        addAll(elements);
+    }
+    public int[] getAsArray(){
+        int[] array = new int[size()];
+        Nodo aux=cabeza;
+        for (int i = 0; i < size(); i++) {
+            array[i] = aux.getElem();
+            aux=aux.getSiguiente();
+        }
+        return array;
+    }
     public int size(){
         return tamaño;
     }
@@ -24,27 +37,96 @@ public class Lista {
     }
     public void addTail(int elem){
         Nodo nodo = new Nodo(elem);
-        if (tamaño==0)
+        if (size()==0)
             cabeza=nodo;
-        else{
-            Nodo nodoAux = cabeza;
-            while (nodoAux.getSiguiente()!=null){
-                nodoAux = nodoAux.getSiguiente();
-            }
-            nodoAux.setSiguiente(nodo);
-        }
+        else
+            cola.setSiguiente(nodo);
+
         cola = nodo;
         tamaño++;
+    }
+    public Integer removeHead(){
+        if (size()==0)
+            return null;
+        int elem = cabeza.getElem();
+        cabeza=cabeza.getSiguiente();
+        tamaño--;
+        
+        if (cabeza==null)
+            cola=null;
+        return elem;
+    }
+    public Integer removeTail(){
+        if(size()==0)
+            return null;
+        if (size()==1)
+            return removeHead();
+        int elem = cola.getElem();
+        Nodo nodoAux = cabeza;
+        for (int i=0;i<size()-1;i++){
+            nodoAux = nodoAux.getSiguiente();
+        }
+        cola=nodoAux;
+        cola.setSiguiente(null);
+        tamaño--;
+        
+        return elem;
+    }
+    public void clear(){
+        tamaño=0;
+        cabeza=null;
+        cola=null;
+    }
+    public boolean contains(int elem){
+        Nodo nodoParam = new Nodo(elem);
+        Nodo nodoAux = cabeza;
+
+        for (int i = 0; i < size(); i++) {
+            if (nodoParam.equals(nodoAux))
+                return true;
+            nodoAux = nodoAux.getSiguiente();
+        }
+        return false;
+    }
+    public Integer get(int posición){
+        if (posición<0 || posición>=size())
+            return null;
+        Nodo nodoAux = cabeza;
+        for (int i = 0; i < posición; i++) {
+            nodoAux = nodoAux.getSiguiente();
+        }
+        return nodoAux.getElem();
+    }
+    public Integer remove(int posición){
+        if (posición<0 || posición>=size())
+            return null;
+        if (posición==0)
+            return removeHead();
+        if (posición==size()-1)
+            return removeTail();
+        int elemBorrado;
+        Nodo nodoAux = cabeza;
+        for (int i = 0; i < posición-1; i++) {
+            nodoAux = nodoAux.getSiguiente();
+        }
+        elemBorrado=nodoAux.getSiguiente().getElem();
+        nodoAux.setSiguiente(nodoAux.getSiguiente().getSiguiente());
+        tamaño--;
+        return elemBorrado;
+    }
+    public void addAll(int[] elements){
+        for (int element:elements)
+            addTail(element);
     }
     @Override
     public String toString(){
         Nodo nodoAux = cabeza;
         String cadena = "Lista con "+tamaño+" elementos: ";
-        while (nodoAux!=null){
+        for (int i=0;i<size();i++){
             cadena += nodoAux.toString()+" -> ";
             nodoAux = nodoAux.getSiguiente();
         }
-        return cadena.substring(0,cadena.length()-4);
+        return cadena.substring(0,size()>0?cadena.length()-4:cadena.length()-2); // Si size>1 la cadena tiene " -> " al final
     }
     private class Nodo{
         private int elem;
@@ -64,6 +146,15 @@ public class Lista {
         @Override
         public String toString(){
             return String.valueOf(elem);
+        }
+        @Override
+        public boolean equals(Object object){
+            if(object==null)
+                return false;
+            if(!(object instanceof Nodo))
+                return false;
+            Nodo nodo = (Nodo) object;
+            return getElem()==nodo.getElem();
         }
     }
 }
--- src/main/java/es/ieslavereda/Main.java
+++ src/main/java/es/ieslavereda/Main.java
@@ -5,10 +5,34 @@ public class Main {
         Lista listaEnteros = new Lista();
         listaEnteros.addHead(4);
         listaEnteros.addHead(7);
+        listaEnteros.addHead(4);
+        listaEnteros.addHead(7);
         listaEnteros.addHead(5);
-        listaEnteros.addHead(9);
         listaEnteros.addTail(9);
+        listaEnteros.addHead(5);
+        listaEnteros.addTail(8);
 
         System.out.println(listaEnteros);
-    }
+        System.out.println("Último elemento de la lista: " + listaEnteros.get(listaEnteros.size()-1));
+        System.out.println("La lista de enteros contiene el número 9?: " + listaEnteros.contains(9));
+
+        listaEnteros.removeHead();
+        listaEnteros.removeTail();
+
+        System.out.println(listaEnteros);
+        System.out.println("La lista de enteros contiene el número 9?: " + listaEnteros.contains(9));
+
+        listaEnteros.clear();
+
+        System.out.println(listaEnteros);
+        System.out.println("La lista de enteros contiene el número 9?: " + listaEnteros.contains(9));
+
+        Lista listaUnElem = new Lista();
+        listaUnElem.addHead(6);
+        listaUnElem.addHead(6);
+        listaUnElem.addHead(6);
+        listaUnElem.addHead(6);
+        listaUnElem.remove(2);
+        System.out.println(listaUnElem);
+        }
 }
