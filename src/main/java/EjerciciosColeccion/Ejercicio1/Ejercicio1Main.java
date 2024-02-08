a9bbaa7949c677d2d0d7d408e2f248d6644a7baf
MarcosGit2020 <marrubdia@alu.edu.gva.es>
2024-02-08 17:59:52 +0100

    0.1

--- src/main/java/EjerciciosColeccion/Ejercicio1/Ejercicio1Main.java
+++ src/main/java/EjerciciosColeccion/Ejercicio1/Ejercicio1Main.java
@@ -2,6 +2,7 @@ package EjerciciosColeccion.Ejercicio1;
 
 import java.time.LocalDate;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Ejercicio1Main {
     public static void main(String[] args) {
@@ -19,61 +20,87 @@ public class Ejercicio1Main {
         for (Pais pais:coleccion){
             System.out.println(pais.getNombre());
         }
-        System.out.println("Set de todos los paises: ");
+
+        System.out.println("\nSet de todos los paises: ");
         for (Pais pais:setPaises){
             System.out.println(pais.getNombre());
         }
 
         System.out.println("\nArrayList de todos los trabajadores ordenados: ");
         ArrayList<Trabajador> listaTrabajadores = new ArrayList<>(mapaTrabajadorPais.keySet());
-        Collections.sort(listaTrabajadores);
-        for (Trabajador trabajador:listaTrabajadores){
-            System.out.println(" --> " + trabajador.getNombre() + " " + trabajador.getApellido());
-        }
-        System.out.println("TreeSet de los trabajadores: ");
+//        Collections.sort(listaTrabajadores);
+//        for (Trabajador trabajador:listaTrabajadores){
+//            System.out.println(" --> " + trabajador.getNombre() + " " + trabajador.getApellido());
+//        }
+        listaTrabajadores.stream().sorted(Trabajador::compareTo).forEach(System.out::println);
+
+        System.out.println("\nTreeSet de los trabajadores: ");
+//        TreeSet<Trabajador> treeSetTrabajadores = new TreeSet<>(mapaTrabajadorPais.keySet());
+//        for (Trabajador trabajador:treeSetTrabajadores){
+//            System.out.println(" --> " + trabajador.getNombre() + " " + trabajador.getApellido());
+//        }
         TreeSet<Trabajador> treeSetTrabajadores = new TreeSet<>(mapaTrabajadorPais.keySet());
-        for (Trabajador trabajador:treeSetTrabajadores){
-            System.out.println(" --> " + trabajador.getNombre() + " " + trabajador.getApellido());
-        }
+        treeSetTrabajadores.forEach(System.out::println); // se ordena primero por apellido que por nombre
+
+
+//        System.out.println("\nListado de los paises ordenados alfabéticamente: ");
+//        TreeSet<Pais> setOrdenadoPaises = new TreeSet<>(mapaTrabajadorPais.values());
+//        for (Pais pais:setOrdenadoPaises){
+//            System.out.println(pais);
+//        }
+//        System.out.println("\nListado trabajadores ordenados por edad: ");
+//        ArrayList<Trabajador> listaTrabajadoresOrdenadosEdad = new ArrayList<>(mapaTrabajadorPais.keySet());
+//        listaTrabajadoresOrdenadosEdad.sort(Trabajador.SORT_BY_AGE);
+//        for (Trabajador trabajador:listaTrabajadoresOrdenadosEdad){
+//            System.out.println(trabajador + " Pais: " + mapaTrabajadorPais.get(trabajador));
+//        }
 
         System.out.println("\nListado de los paises ordenados alfabéticamente: ");
-        TreeSet<Pais> setOrdenadoPaises = new TreeSet<>(mapaTrabajadorPais.values());
-        for (Pais pais:setOrdenadoPaises){
-            System.out.println(pais);
-        }
-        System.out.println("Listado trabajadores ordenados por edad: ");
-        ArrayList<Trabajador> listaTrabajadoresOrdenadosEdad = new ArrayList<>(mapaTrabajadorPais.keySet());
-        listaTrabajadoresOrdenadosEdad.sort(Trabajador.SORT_BY_AGE);
-        for (Trabajador trabajador:listaTrabajadoresOrdenadosEdad){
-            System.out.println(trabajador + " Pais: " + mapaTrabajadorPais.get(trabajador));
-        }
+        TreeSet<Pais> setOrdenadoPaisesStream = new TreeSet<>(mapaTrabajadorPais.values());
+        setOrdenadoPaisesStream.stream().sorted((p1,p2)->p1.getNombre().compareTo(p2.getNombre()))
+                .map(p -> p.getNombre()).forEach(System.out::println);
+        System.out.println("\nListado trabajadores ordenados por edad: ");
+        mapaTrabajadorPais.keySet().stream().sorted((t1,t2)->(int)(t2.getEdad()- t1.getEdad())).forEach(System.out::println);
 
-        System.out.println("\nListado de cada pais y de cada uno de sus trabajadores: ");
-        TreeMap<Pais,ArrayList<Trabajador>> mapaPaisYTrabajadores = new TreeMap<>();
-        for (Pais pais:setOrdenadoPaises){ // No se repiten
-            ArrayList<Trabajador> trabajadoresPertenecenPais = new ArrayList<>();
-            for (Trabajador trabajador:mapaTrabajadorPais.keySet()){
-                if (pais.equals(mapaTrabajadorPais.get(trabajador))){
-                    trabajadoresPertenecenPais.add(trabajador);
-                }
-            }
-            trabajadoresPertenecenPais.sort(Trabajador.SORT_BY_AGE);
-            mapaPaisYTrabajadores.put(pais,trabajadoresPertenecenPais);
-        }
-        for (Pais pais:mapaPaisYTrabajadores.keySet()){ // Sacar por consola
-            System.out.println("Trabajadores de " + pais.getNombre() + ": ");
-            for (Trabajador trabajador:mapaPaisYTrabajadores.get(pais)){
-                System.out.println(" " + trabajador);
-            }
-            System.out.println("----------------------------------------------------------------------------");
-        }
 
-        System.out.println("\nTrabajadores de mas de 50 años: ");
-        Iterator<Trabajador> iteradorTrabajador = mapaTrabajadorPais.keySet().iterator();
-        while (iteradorTrabajador.hasNext()){
-            Trabajador trabajador = iteradorTrabajador.next();
-            if (trabajador.getEdad()>50)
-                System.out.println(" "+trabajador);
+//        System.out.println("\nListado de cada pais y de cada uno de sus trabajadores: ");
+//        TreeMap<Pais,ArrayList<Trabajador>> mapaPaisYTrabajadores = new TreeMap<>();
+//        for (Pais pais:setOrdenadoPaises){ // No se repiten
+//            ArrayList<Trabajador> trabajadoresPertenecenPais = new ArrayList<>();
+//            for (Trabajador trabajador:mapaTrabajadorPais.keySet()){
+//                if (pais.equals(mapaTrabajadorPais.get(trabajador))){
+//                    trabajadoresPertenecenPais.add(trabajador);
+//                }
+//            }
+//            trabajadoresPertenecenPais.sort(Trabajador.SORT_BY_AGE);
+//            mapaPaisYTrabajadores.put(pais,trabajadoresPertenecenPais);
+//        }
+//        for (Pais pais:mapaPaisYTrabajadores.keySet()){ // Sacar por consola
+//            System.out.println("Trabajadores de " + pais.getNombre() + ": ");
+//            for (Trabajador trabajador:mapaPaisYTrabajadores.get(pais)){
+//                System.out.println(" " + trabajador);
+//            }
+//            System.out.println("----------------------------------------------------------------------------");
+//        }
+
+        String cad = "\nListado de cada pais y de cada uno de sus trabajadores con stream:\n";
+        for (Pais pais:new TreeSet<>(mapaTrabajadorPais.values())){
+            cad += pais.toString();
+            cad += "\n" + mapaTrabajadorPais.keySet().stream().filter(t -> mapaTrabajadorPais.get(t).equals(pais))
+                    .sorted((t1,t2)->(int)(t1.getEdad()-t2.getEdad())).map(t->t.getNombre())
+                    .collect(Collectors.joining(", ","Trabajadores de " + pais.getNombre() + ": ",".\n"));
         }
+        System.out.println(cad);
+
+//        System.out.println("\nTrabajadores de mas de 50 años: ");
+//        Iterator<Trabajador> iteradorTrabajador = mapaTrabajadorPais.keySet().iterator();
+//        while (iteradorTrabajador.hasNext()){
+//            Trabajador trabajador = iteradorTrabajador.next();
+//            if (trabajador.getEdad()>50)
+//                System.out.println(" "+trabajador);
+//        }
+
+        System.out.println("\nTrabajadores de mas de 50 años con stream: ");
+        mapaTrabajadorPais.keySet().stream().filter(trabajador -> trabajador.getEdad()>50).forEach(System.out::println);
     }
 }
